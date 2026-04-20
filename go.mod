7b17e74b2a4a73ad3e81bb11bce788dcbfee6e53
codeBuddha <jamwujustyle@gmail.com>
2026-04-20 18:50:18 +0500

    sync

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jamwujustyle/logger
+
+go 1.25.5
---
+++ logger.go
@@ -0,0 +1,29 @@
+package logger
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"path/filepath"
+)
+
+func InitLogger() {
+	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level:     slog.LevelDebug,
+		AddSource: true,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.TimeKey {
+				return slog.String(a.Key, a.Value.Time().Format("15:04:05"))
+			}
+			if a.Key == slog.SourceKey {
+				source := a.Value.Any().(*slog.Source)
+
+				dir := filepath.Base(filepath.Dir(source.File))
+				file := filepath.Base(source.File)
+				return slog.String(a.Key, fmt.Sprintf("%s/%s:%d", dir, file, source.Line))
+			}
+			return a
+		},
+	})
+	slog.SetDefault(slog.New(handler))
+}
