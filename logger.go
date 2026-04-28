16e97cebd91f511c45a60bad08be070616604cc0
codeBuddha <jamwujustyle@gmail.com>
2026-04-28 19:07:29 +0500

    update func to accept a time bool

--- logger.go
+++ logger.go
@@ -7,12 +7,15 @@ import (
 	"path/filepath"
 )
 
-func InitLogger() {
+func InitLogger(withTime bool) {
 	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		Level:     slog.LevelDebug,
 		AddSource: true,
 		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
 			if a.Key == slog.TimeKey {
+				if !withTime {
+					return slog.Attr{}
+				}
 				return slog.String(a.Key, a.Value.Time().Format("15:04:05"))
 			}
 			if a.Key == slog.SourceKey {
